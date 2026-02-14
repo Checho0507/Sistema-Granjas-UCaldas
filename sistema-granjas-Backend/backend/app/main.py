0d712fe09a0bc0c4a555f0e5c979b36ec8f90d57
Checho0507 <checho.mlj@gmail.com>
2026-02-14 16:47:19 -0500

    borrando http

--- sistema-granjas-Backend/backend/app/main.py
+++ sistema-granjas-Backend/backend/app/main.py
@@ -76,7 +76,7 @@ app = FastAPI(
 # CORS - Añade tu dominio de frontend si está en producción
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173", "https://sistemagranjasucaldas-production.up.railway.app"],
+    allow_origins=["https://sistemagranjasucaldas-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
