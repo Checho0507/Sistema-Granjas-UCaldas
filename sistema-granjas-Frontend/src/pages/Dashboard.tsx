1f4da69440f1e556696121b23848e0a2a47a715c
Checho0507 <checho.mlj@gmail.com>
2026-03-16 17:54:20 -0500

    correct import

--- sistema-granjas-Frontend/src/pages/Dashboard.tsx
+++ sistema-granjas-Frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DashboardHeader from '../components/Common/DashboardHeader';
-import Sidebar from '../components/Common/Sidebar'; // Nota: importa Sidebar, no SideBar
+import Sidebar from '../components/Common/SideBar'; // Nota: importa Sidebar, no SideBar
 import granjaService from '../services/granjaService';
 import programaService from '../services/programaService';
 import loteService from '../services/loteService';
 import { normalizarArray } from '../utils/normalize';
-import type { Granja, Programa, Lote } from '../types/granjaTypes';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
